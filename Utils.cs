62cf926a1506ee34cdeaa173d103df49ce754b01
byghost4 <1072872260@qq.com>
2019-07-04 10:26:23 +0800

    创建

---
+++ Utils.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utils
+{
+    public static string ChineseTONumber(string chineseStr1)
+    {
+        string numStr = "0123456789";
+        string chineseStr = "零一二三四五六七八九";
+        char[] c = chineseStr1.ToCharArray();
+        for (int i = 0; i < c.Length; i++)
+        {
+            int index = chineseStr.IndexOf(c[i]);
+            if (index != -1)
+                c[i] = numStr.ToCharArray()[index];
+        }
+        numStr = null;
+        chineseStr = null;
+        return new string(c);
+    }
+    public static string NumberToChinese(string numberStr)
+    {
+        string numStr = "0123456789";
+        string chineseStr = "零一二三四五六七八九";
+        char[] c = numberStr.ToCharArray();
+        for (int i = 0; i < c.Length; i++)
+        {
+            int index = numStr.IndexOf(c[i]);
+            if (index != -1)
+                c[i] = chineseStr.ToCharArray()[index];
+        }
+        numStr = null;
+        chineseStr = null;
+        return new string(c);
+    } 
+}
